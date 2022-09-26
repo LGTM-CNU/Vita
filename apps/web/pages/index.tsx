dfa69b5a578a4428231072cdf28044a80e6fb2f9
hyunjin <leehj0110@kakao.com>
2022-09-26 19:24:57 +0900

    feat: 메인화면 마크업

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
 import styled from "styled-components"
 import { Button } from "ui"
 
 export default function Web() {
+  const [url, setUrl] = useState("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value)
+  }
+
   return (
     <Container>
-      <h1>Web</h1>
-      <Button />
+      <h1>Vita</h1>
+
+      <input type="text" placeholder="Vita URL" onChange={handleChange} />
+
+      <Button>사용자로 로그인</Button>
+      <Button>관리자로 로그인</Button>
     </Container>
   )
 }
 
-const Container = styled.div``
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
