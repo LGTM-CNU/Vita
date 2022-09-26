daed9f9586cc59f2627ea3dc69bd4254415f0f01
hyunjin <leehj0110@kakao.com>
2022-09-26 19:25:16 +0900

    feat: 공통 버튼 정의

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,9 @@
-import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
-};
+import * as React from "react"
+
+interface Props {
+  children: React.ReactNode
+}
+
+export const Button: React.FC<Props> = ({ children }) => {
+  return <button>{children}</button>
+}
