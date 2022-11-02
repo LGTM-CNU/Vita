de093053e0c798322076535be595672fcfa45155
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-11-02 19:42:45 +0900

    chore: code 포맷 수정

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -1,22 +1,18 @@
-import Image from "next/image"
-import styled from "styled-components"
-import Vita from "../public/logo.png"
+import Image from "next/image";
+import styled from "styled-components";
+import Vita from "../public/logo.png";
 
 export default function Header() {
   return (
     <Container>
-      <Image src={Vita} width={100} height={50} alt="Vita" />
+      <Image src={Vita} width={200} height={100} alt="Vita" />
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
 
-  border-bottom: 0.1rem solid grey;
-
   margin-bottom: 1rem;
-`
-
-// FC7522
+`;
