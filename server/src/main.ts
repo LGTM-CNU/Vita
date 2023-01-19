35988a2937085f146a7711f4b4171b724a7413ad
hyunjin <leehj0110@kakao.com>
2023-01-19 13:38:58 +0900

    chore: add express session

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use();
+  app.use(
+    session({
+      secret: 'hyunjin',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000, // 1분
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
