3e8a0a72d54bfce6c8f53ee9fb7a9254e57e184b
hyunjin <leehj0110@kakao.com>
2023-03-12 12:21:02 +0900

    docs: add readme

--- scripts/speaker.py
+++ scripts/speaker.py
@@ -1,8 +1,6 @@
 # speaker-test -t wav
 # 위 명령어를 통해 스피커를 테스트 할 수 있다.
-
 # ref
 # https://github.com/HUST-Robot/Capstone-design/issues/13
-
 # 마이크
 # https://yong-it.blogspot.com/2016/02/raspberry-pi.html?m=0
