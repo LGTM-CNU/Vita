0b0c4fba146c49beb311f5d423d7556a95c1aa7e
hyunjin <leehj0110@kakao.com>
2023-05-08 10:39:50 +0900

    feat: mutex를 사용해 공유자원에 대한 안정성 보장

--- scripts/producer-consumer.py
+++ scripts/producer-consumer.py
@@ -1,16 +1,21 @@
 import threading
-value = 0
 
+mutex = threading.Lock()
+value = 0
 
 def plus():
     global value
     for i in range(1000000):
+      mutex.acquire()
       value += 1
+      mutex.release()
 
 def minus():
     global value
     for i in range(1000000):
+      mutex.acquire()
       value -= 1
+      mutex.release()
 
 producer_thread = threading.Thread(target=plus)
 producer_thread.start()
@@ -18,5 +23,10 @@ producer_thread.start()
 consumer_thread = threading.Thread(target=minus)
 consumer_thread.start()
 
+producer_thread.join()
+consumer_thread.join()
 
 print(value)
+
+# mutex를 활용해서 공유자원에 대한 접근을 제어한다.
+# 스레드 간의 안정성을 보장한다.
